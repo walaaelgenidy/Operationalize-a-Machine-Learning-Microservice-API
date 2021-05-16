8f5f2436ca8556b1bae27266ad950d26b1594acc
walaaelgenidy <45464802+walaaelgenidy@users.noreply.github.com>
2021-05-16 16:22:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 @app.route("/")
 def home():
     html = f"<h3>Sklearn Prediction Home</h3>"
-    return html.format(format)
+    return html.format()
 
 @app.route("/predict", methods=['POST'])
 def predict():
